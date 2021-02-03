561bb16f3d6b02263ffd660112d5460b9a602d10
antanasbendoraitis <antanasbendoraitis7@gmail.com>
2021-02-03 20:06:32 +0200

    Add project files.

---
+++ Baldu gamybos Informacine sistema/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Baldu_gamybos_Informacine_sistema.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
